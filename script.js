6795cacbde2d75daec19ec4b37091011ea51513a
Deepak Sharma <dsharma2828@gmail.com>
2024-05-02 17:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	if (!s.includes(' ')) {
+        return s;
+    }
+    return s.substring(0, s.indexOf(' '));
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
